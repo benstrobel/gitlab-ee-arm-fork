9ba44585040ef4b176947dd66417b4a8bc38e565
Clemens Beck <cbeck@gitlab.com>
2023-08-28 16:38:05 +0200

    Stub 16.4.0 version info
    This is to build a package/image with stubbed 16.4.0 EE version info,
    to test the updated upgrade check behaviour.
    
    This should NOT be merged with master or any stable branch.

--- lib/gitlab/build/info/git.rb
+++ lib/gitlab/build/info/git.rb
@@ -37,54 +37,11 @@ module Build
         # TODO, merge latest_tag with latest_stable_tag
         # TODO, add tests, needs a repo clone
         def latest_tag
-          unless (fact_from_file = Gitlab::Util.fetch_fact_from_file(__method__)).nil?
-            return fact_from_file
-          end
-
-          tags = sorted_tags_for_edition
-
-          return if tags.empty?
-
-          version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
-          output = tags.find { |t| t.start_with?(version) } if version
-
-          # If no tags corresponding to the stable branch version was found, we
-          # fall back to the latest available tag
-          output || tags.first
+          '16.4.0+ee.0'
         end
 
         def latest_stable_tag(level: 1)
-          unless (fact_from_file = Gitlab::Util.fetch_fact_from_file(__method__)).nil?
-            return fact_from_file
-          end
-
-          # Exclude RC tags so that we only have stable tags.
-          stable_tags = sorted_tags_for_edition.reject { |t| t.include?('rc') }
-
-          return if stable_tags.empty?
-
-          version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
-
-          results = stable_tags.select { |t| t.start_with?(version) } if version
-
-          # If no tags corresponding to the stable branch version was found, we
-          # fall back to the latest available stable tag
-          output = if results.nil? || results.empty?
-                     stable_tags
-                   else
-                     results
-                   end
-
-          # Level decides tag at which position you want. Level one gives you
-          # latest stable tag, two gives you the one just before it and so on.
-          # Since arrays start from 0, we subtract 1 from the specified level to
-          # get the index. If the specified level is more than the number of
-          # tags, we return the last tag.
-          if level >= output.length
-            output.last
-          else
-            output[level - 1]
-          end
+          '16.4.0+ee.0'
         end
 
         private
