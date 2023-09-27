d55e51830bc68d87c2b79b2c1521b24a3bc14917
Amaury Yacksmith <myacksmith@gitlab.com>
2023-09-27 16:23:34 -0400

    Cache gitlab-ctl help cmds for faster results

--- support/gitlab-ctl_completions.sh
+++ support/gitlab-ctl_completions.sh
@@ -8,15 +8,20 @@ _gitlab-ctl_completions()
         return
     fi
 
-    # Regex looks for lines:
-    #  1. Starting with 0 or more whitespaces
-    #  2. Followed by at least one lowercase word
-    #  3. Ending with zero or more lowercase words separate by dashes.
-    COMPREPLY=($(compgen -W \
-        "$(gitlab-ctl --help \
-        | awk '/^ *[a-z]+(-[a-z]*)*$/
-            { gsub(/ /, ""); print }'
-        )" -- "${COMP_WORDS[1]}"))
+    # Check if gitlab-ctl compspec already exists
+    if [ -z "${GITLAB_CTL_CMDS}" ]; then
+        # if not, register them.
+        GITLAB_CTL_CMDS="$(gitlab-ctl --help \
+            # Regex looks for lines:
+            #  1. Starting with 0 or more whitespaces
+            #  2. Followed by at least one lowercase word
+            #  3. Ending with zero or more lowercase words 
+            #     separated by dashes.
+            | awk '/^ *[a-z]+(-[a-z]*)*$/ { gsub(/ /, ""); print }'
+            )"
+    fi
+
+    COMPREPLY=($(compgen -W "${GITLAB_CTL_CMDS}" -- "${COMP_WORDS[1]}"))
 }
 
 complete -F _gitlab-ctl_completions gitlab-ctl
